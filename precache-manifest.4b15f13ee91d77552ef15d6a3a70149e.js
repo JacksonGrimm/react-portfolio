35b4c261ec639ac4ab889f0429d85357c4c3e329
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2023-01-13 11:57:04 -0700

    Updates

---
+++ precache-manifest.4b15f13ee91d77552ef15d6a3a70149e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a95d6f79e90825ea691",
+    "url": "/react-portfolio/static/css/main.669d097d.chunk.css"
+  },
+  {
+    "revision": "0a95d6f79e90825ea691",
+    "url": "/react-portfolio/static/js/main.0a95d6f7.chunk.js"
+  },
+  {
+    "revision": "4c984d63551f9bd37749",
+    "url": "/react-portfolio/static/js/1.4c984d63.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "3fe8d0cb44f4b9b896164ad6d185d0c2",
+    "url": "/react-portfolio/static/media/icons8-stack.3fe8d0cb.svg"
+  },
+  {
+    "revision": "638e7af0ec4a8a4ec4a9d675e52584c4",
+    "url": "/react-portfolio/static/media/PWA.638e7af0.png"
+  },
+  {
+    "revision": "77f47cc08cf34f6fe271f95dbfee97d6",
+    "url": "/react-portfolio/static/media/quiz-app.77f47cc0.png"
+  },
+  {
+    "revision": "dd65b9e96def9b620a6234c818b52609",
+    "url": "/react-portfolio/static/media/Social-media-backend.dd65b9e9.png"
+  },
+  {
+    "revision": "6df7ed0a9a8c13d751324e480ffbdb33",
+    "url": "/react-portfolio/static/media/weather-app.6df7ed0a.png"
+  },
+  {
+    "revision": "e9dd9373bd88047bf46fa10078743dcf",
+    "url": "/react-portfolio/static/media/wave-2.e9dd9373.svg"
+  },
+  {
+    "revision": "5c333285cf6d6d525de0c21abf798cad",
+    "url": "/react-portfolio/static/media/wave-1.5c333285.svg"
+  },
+  {
+    "revision": "d2596a8bb581cf77d673095988bee036",
+    "url": "/react-portfolio/static/media/mountains.d2596a8b.jpg"
+  },
+  {
+    "revision": "20c124ec7f5044712a2d8924b5c22f1e",
+    "url": "/react-portfolio/static/media/pawsome.20c124ec.png"
+  },
+  {
+    "revision": "17f8b1a808edae01e9a48690813cb569",
+    "url": "/react-portfolio/static/media/NPS.17f8b1a8.png"
+  },
+  {
+    "revision": "ecdc1125a94d716dd8b2cd1fe5c466e6",
+    "url": "/react-portfolio/static/media/MEMoji.ecdc1125.PNG"
+  },
+  {
+    "revision": "4f75e58c5bf79ab28001287fcece41c9",
+    "url": "/react-portfolio/index.html"
+  }
+];
